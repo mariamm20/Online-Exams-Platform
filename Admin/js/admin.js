76e763846ffc67dc1da3fc234b99547eb99864fd
mariamm20 <mero5691@gmail.com>
2022-04-24 07:27:18 +0200

    admin responsive

--- Admin/js/admin.js
+++ Admin/js/admin.js
@@ -1,13 +1,23 @@
 let ul = document.querySelector('.elements');
-if(screen.width <= 700){
-document.onload(ul.classList.add('collapse'));
+if (screen.width <= 700) {
+    document.onload(ul.classList.add('collapse'));
 }
 
+let modals = document.querySelectorAll('.modal');
+let bodyy = document.querySelector('.table-content');
 
+window.onclick = e => {
+    if (e.target.tagName === 'I') {
+        for (let modal of modals) {
+            if (modal.classList.contains('show')) {
+                bodyy.scrollLeft=0;
+                bodyy.style.scrollBehavior = "smooth";
+                bodyy.classList.add('hide');
+            } 
+        }
 
-
-
-
-
-
-
+    }
+else{
+    bodyy.classList.remove('hide');
+}
+}
