94aa71dc548b3a7bbc53dba9e456e172c71a9fdd
mariamm20 <mero5691@gmail.com>
2022-05-15 18:58:48 +0200

    rest of student pages

---
+++ Stud-Subjects/js/stud-subjects.js
@@ -0,0 +1,11 @@
+function show(){
+    let x = document.querySelector(".img-right")
+    let y = document.querySelector(".chapters")
+    let z = document.querySelector(".names-chapters")
+
+    x.classList.add("d-none")
+    y.classList.add("magictime","swashIn")
+    y.classList.remove("d-none")
+    z.classList.add("magictime","swashIn")
+    z.classList.remove("d-none")
+}
