240b6b9c29cf8693327cbf445ea6171a81779222
Your Name~ <alaaattia@gmail.com>
2022-05-20 14:40:01 +0200

    edit

--- online_exam_platform.sql
+++ online_exam_platform.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2022 at 05:18 PM
+-- Generation Time: May 20, 2022 at 02:37 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -34,6 +34,36 @@ CREATE TABLE `answers` (
   `is_correct` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`id`, `question_id`, `answer`, `is_correct`) VALUES
+(375, 158, '1', 0),
+(376, 158, '2', 1),
+(377, 158, '3', 0),
+(378, 158, '4', 0),
+(379, 159, 'task focused', 0),
+(380, 159, 'disinterested', 0),
+(381, 159, 'supportive', 1),
+(382, 159, 'process oriented', 0),
+(383, 160, 'connective', 0),
+(384, 160, 'contemplative', 0),
+(385, 160, 'collaborative', 1),
+(386, 160, 'connectivity', 0),
+(387, 161, 'True', 1),
+(388, 161, 'False', 0),
+(389, 162, 'True', 1),
+(390, 162, 'False', 0),
+(391, 163, 'True', 0),
+(392, 163, 'False', 1),
+(393, 164, 'True', 0),
+(394, 164, 'False', 1),
+(395, 165, 'True', 1),
+(396, 165, 'False', 0),
+(397, 166, 'True', 0),
+(398, 166, 'False', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +75,16 @@ CREATE TABLE `chapters` (
   `subject_id` int(11) NOT NULL,
   `chapter_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chapters`
+--
+
+INSERT INTO `chapters` (`id`, `subject_id`, `chapter_name`) VALUES
+(17, 18, 'chapter one'),
+(18, 18, 'chapter two'),
+(19, 18, 'chapter three'),
+(20, 18, 'chapter four');
 
 -- --------------------------------------------------------
 
@@ -60,6 +100,15 @@ CREATE TABLE `contact` (
   `state` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `msg`, `state`) VALUES
+(8, 'amr abohany', 'amr@gmail.com', 'this site is beautiful', NULL),
+(9, 'alaa attia', 'alaaattia147@gmail.com', 'i have a problem in my life', NULL),
+(10, 'alaa attia', 'alaaattia147@gmail.com', 'dfghjkl', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -71,6 +120,17 @@ CREATE TABLE `departments` (
   `dept_name` varchar(100) NOT NULL,
   `level_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `dept_name`, `level_id`) VALUES
+(18, 'General', 13),
+(19, 'General', 14),
+(20, 'SE', 15),
+(21, 'CS', 15),
+(22, 'IS', 15);
 
 -- --------------------------------------------------------
 
@@ -88,6 +148,16 @@ CREATE TABLE `exams` (
   `total_mark` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `exams`
+--
+
+INSERT INTO `exams` (`id`, `subject_id`, `exam_name`, `exam_date`, `start_time`, `duration`, `total_mark`) VALUES
+(16, 18, 'Quiz_1', '2022-05-18', '18:00:00', 20, 12),
+(17, 18, 'Exam 2', '2022-05-24', '16:14:00', 15, 6),
+(18, 18, 'exam 3', '2022-05-06', '10:48:00', 20, 6),
+(19, 18, 'Exam 4', '2022-05-21', '09:25:00', 15, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -103,6 +173,27 @@ CREATE TABLE `exam_structure` (
   `difficulty` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `exam_structure`
+--
+
+INSERT INTO `exam_structure` (`id`, `exam_id`, `chapter_id`, `num_of_questions`, `type`, `difficulty`) VALUES
+(157, 16, 17, 1, 'choice', 'easy'),
+(158, 16, 17, 1, 'choice', 'medium'),
+(159, 16, 17, 1, 'choice', 'hard'),
+(160, 16, 17, 1, 'true&false', 'easy'),
+(161, 16, 17, 1, 'true&false', 'medium'),
+(162, 16, 17, 1, 'true&false', 'hard'),
+(163, 17, 17, 1, 'true&false', 'easy'),
+(164, 17, 17, 1, 'true&false', 'medium'),
+(165, 17, 17, 1, 'true&false', 'hard'),
+(166, 18, 17, 1, 'choice', 'easy'),
+(167, 18, 17, 1, 'choice', 'medium'),
+(168, 18, 17, 1, 'choice', 'hard'),
+(169, 19, 17, 1, 'choice', 'easy'),
+(170, 19, 17, 1, 'choice', 'medium'),
+(171, 19, 17, 0, 'choice', 'hard');
+
 -- --------------------------------------------------------
 
 --
@@ -113,6 +204,15 @@ CREATE TABLE `levels` (
   `id` int(11) NOT NULL,
   `level_name` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `levels`
+--
+
+INSERT INTO `levels` (`id`, `level_name`) VALUES
+(13, 'one'),
+(14, 'two'),
+(15, 'three');
 
 -- --------------------------------------------------------
 
@@ -129,8 +229,16 @@ CREATE TABLE `professors` (
   `role` varchar(15) NOT NULL,
   `state` tinyint(1) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `login_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `login_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `professors`
+--
+
+INSERT INTO `professors` (`id`, `user_name`, `academic_id`, `email`, `password`, `role`, `state`, `created_at`, `login_at`, `image`) VALUES
+(10, 'amr ali', '1618151416121454', 'amr@gmail.com', '$2y$10$bwwwiB4C1Px4AKdYFWjJG.DJkVDTQKiDUmxuRVnYRSDBpedS4u8nK', 'professor', 1, '2022-05-16 15:47:37', '2022-05-20 12:31:22', '');
 
 -- --------------------------------------------------------
 
@@ -142,6 +250,13 @@ CREATE TABLE `professor_subjects` (
   `prof_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `professor_subjects`
+--
+
+INSERT INTO `professor_subjects` (`prof_id`, `subject_id`) VALUES
+(10, 18);
 
 -- --------------------------------------------------------
 
@@ -159,6 +274,21 @@ CREATE TABLE `questions` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `subject_id`, `chapter_id`, `question_text`, `type`, `difficulty`, `created_at`) VALUES
+(158, NULL, 17, 'how many types of requirements?', 'choice', 'easy', '2022-05-16 16:12:39'),
+(159, NULL, 17, 'is a charachteristic of an agile leader?', 'choice', 'medium', '2022-05-16 16:16:07'),
+(160, NULL, 17, 'which of the following is the working calture of an agile method?', 'choice', 'hard', '2022-05-16 16:18:05'),
+(161, NULL, 17, 'is there any difference betweem incremental and iterative development?', 'true&false', 'easy', '2022-05-16 16:19:14'),
+(162, NULL, 17, 'waterfall model is useful in large projects?', 'true&false', 'medium', '2022-05-16 16:19:55'),
+(163, NULL, 17, 'code and fix is an agile development model?', 'true&false', 'hard', '2022-05-16 16:21:10'),
+(164, NULL, 17, 'is agile useless in large projects?', 'true&false', 'medium', '2022-05-18 06:45:33'),
+(165, NULL, 18, 'does sc refer to softwre construction?', 'true&false', 'easy', '2022-05-20 05:41:22'),
+(166, NULL, 17, 'is it write for your code to be not clean?', 'true&false', 'medium', '2022-05-20 06:06:35');
+
 -- --------------------------------------------------------
 
 --
@@ -171,6 +301,15 @@ CREATE TABLE `results` (
   `exam_id` int(11) NOT NULL,
   `result` int(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `results`
+--
+
+INSERT INTO `results` (`id`, `student_id`, `exam_id`, `result`) VALUES
+(23, 11, 16, 10),
+(24, 11, 17, 6),
+(25, 11, 18, 2);
 
 -- --------------------------------------------------------
 
@@ -190,8 +329,16 @@ CREATE TABLE `students` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `login_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `level_id` int(11) NOT NULL,
-  `dept_id` int(11) NOT NULL
+  `dept_id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `user_name`, `academic_id`, `email`, `password`, `level`, `department`, `role`, `created_at`, `login_at`, `level_id`, `dept_id`, `image`) VALUES
+(11, 'alaa attia', '1618129105487965', 'alaaattia147@gmail.com', '$2y$10$zaLAYl8.oK/kyqJAMvG5HuuezNscl92XJiTNe9.Q/WLYOnXOEIk0y', 'three', 'SE', 'student', '2022-05-16 15:40:39', '2022-05-20 12:32:00', 15, 20, 'IMG_0434.JPG');
 
 -- --------------------------------------------------------
 
@@ -207,6 +354,24 @@ CREATE TABLE `student_answers` (
   `answer_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `student_answers`
+--
+
+INSERT INTO `student_answers` (`id`, `student_id`, `exam_id`, `question_id`, `answer_id`) VALUES
+(134, 11, 16, 158, 376),
+(135, 11, 16, 159, 381),
+(136, 11, 16, 160, 386),
+(137, 11, 16, 161, 387),
+(138, 11, 16, 162, 389),
+(139, 11, 16, 163, 392),
+(140, 11, 17, 161, 387),
+(141, 11, 17, 162, 389),
+(142, 11, 17, 163, 392),
+(143, 11, 18, 158, 376),
+(144, 11, 18, 159, 380),
+(145, 11, 18, 160, 386);
+
 -- --------------------------------------------------------
 
 --
@@ -219,6 +384,16 @@ CREATE TABLE `subjects` (
   `level_id` int(11) NOT NULL,
   `dept_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subject_name`, `level_id`, `dept_id`) VALUES
+(16, 'privacy', 13, 18),
+(17, 'data base', 14, 19),
+(18, 'software construction', 15, 20),
+(19, 'software requirements', 15, 20);
 
 --
 -- Indexes for dumped tables
@@ -335,79 +510,79 @@ ALTER TABLE `subjects`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=375;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=399;
 
 --
 -- AUTO_INCREMENT for table `chapters`
 --
 ALTER TABLE `chapters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `exams`
 --
 ALTER TABLE `exams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `exam_structure`
 --
 ALTER TABLE `exam_structure`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
 
 --
 -- AUTO_INCREMENT for table `levels`
 --
 ALTER TABLE `levels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `professors`
 --
 ALTER TABLE `professors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=158;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=167;
 
 --
 -- AUTO_INCREMENT for table `results`
 --
 ALTER TABLE `results`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `student_answers`
 --
 ALTER TABLE `student_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
